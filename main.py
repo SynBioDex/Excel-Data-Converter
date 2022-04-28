6bc11a6a82452c096482ef7f85035660117df3b7
saisa <saisam17@gmail.com>
2022-04-28 06:56:50 -0600

    Initial commmit

---
+++ main.py
@@ -0,0 +1,8 @@
+
+#create function that takes the hashmap and the parsed dictionary for type - this will be driver function
+  #for loop to go through each row and call endpoint functino
+     # id = column_fj_func_from_below
+     # create chemical object
+     # hashmap.add (id, chem_obj)
+
+#func that calls flapjack endpoint for submitting column - input will be data submitted to fj and output will be id
