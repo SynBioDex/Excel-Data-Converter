ffd5a407fa82ce8dba3d393f04cbd035a1ebd9b9
saisam17 <saisam17@gmail.com>
2022-09-20 13:35:13 -0600

    Working version, just need to add measurements

--- excel2flapjack/excel2flapjack/main.py
+++ excel2flapjack/excel2flapjack/main.py
@@ -38,8 +38,9 @@ def flapjack_upload(fj_url, fj_user, fj_pass, excel_path):
     # order is important as Chemicals and DNA must be created before
     # they can be referenced
     types = ['Chemical', 'DNA', 'Supplement', 'Vector', 'Strain', 'Media',
-             'Signal', 'Measurement']
-
+             'Signal']
+    #types = ['DNA', 'Supplement', 'Vector', 'Strain', 'Media',
+             #'Signal', 'Measurement']
     # initiate hashmap for linking to chemicals
     hash_map = {}
 
@@ -67,7 +68,7 @@ def flapjack_upload(fj_url, fj_user, fj_pass, excel_path):
         obj_dict = obj_df.to_dict('index')
 
         # REMOVE THIS LINE WHEN USING FLAPJACK
-        fj = Flapjack()
+        #fj = Flapjack()
 
         # Upload all the objects to flapjack
         for key in obj_dict:
@@ -81,10 +82,14 @@ def flapjack_upload(fj_url, fj_user, fj_pass, excel_path):
 
             # CHANGE THIS WHEN USING FLAPJACK
             # add ** infront of data later when not patched!!!!!!!!
+            print(obj)
+            data['model'] = obj.lower()
+            print(data)
             flapjack_id = fj.create(**data)
             # flapjack_id = fj.create(data)
 
             # add Chemical and DNA to hash map to allow cross referencing
+            print(flapjack_id)
             hash_map[key] = flapjack_id.id[0]
 
     return hash_map
