4c52c4d6fe5db1991066e7feb6c15f630b7071c1
dylan33smith <dysm3436@colorado.edu>
2024-03-04 13:03:59 -0700

    converted x2f to class structure (in src/mainNew script). jupyter nb for testing is e2f_oop

---
+++ src/excel2flapjack/mainNew.py
@@ -0,0 +1,267 @@
+import os
+import pandas as pd
+from flapjack import Flapjack
+
+""" Questions:
+    1. what is the hash_map used for?
+        - it says "for linking to chemicals" but linking what to chemicals?
+
+"""
+
+
+
+# class definition
+class X2F:
+
+    """ 
+        inputData retrives information from input XDC excel template, connects to fj, and formats and uploads data
+        includes: 
+            - class definition
+            - initialization method
+            - class attributes
+            - instance attributes
+            - methods
+        
+
+        Instance Attributes
+        ----------
+        fj : flapjack.flapjack.Flapjack object
+            fj case that contains the connection to specific fj account
+
+        xls: pd.ExcelFile
+            pandas excel file
+
+        df : pandas dataframe  
+            contains all the information from XDC file in a format that can be used by FJ
+
+        hash_map: hash map
+            key: name of study attributes from XDC spreadsheet
+            value: corresponding flapjackID  
+
+
+    """
+    # list of all the sheet names in XDC excel file
+    # each will define an fj object with data extracted from the excel sheet
+    # use self.types to access this class attribute
+    types = ['Chemical', 'DNA', 'Supplement', 'Vector', 'Strain', 'Media',
+            'Signal', 'Study', 'Assay', 'Sample', 'Measurement', 'Sample Design']
+
+
+    def __init__(self, 
+                 xls, 
+                 fj_url, 
+                 fj_user, 
+                 fj_pass):
+        # fj url has to include 8000 port
+        if not fj_url.endswith(":8000"):
+            fj_url = fj_url + ":8000"
+
+        self.fj = Flapjack(url_base=fj_url)
+        self.fj.log_in(username = fj_user, password = fj_pass)
+        self.fj_conv_sht = xls.parse('FlapjackCols', skiprows=0)
+        self.xls = xls
+        self.df = pd.DataFrame()
+        self.hash_map = {}
+
+    def print_info(self, fj_sht = False, df=False, hash_map=False):
+        if fj_sht:
+            print(self.fj_conv_sht)
+        if df:
+            print(self.df.head())
+            print(self.df.info())
+        if hash_map:
+            print(self.hash_map)
+
+    def create_df(self):
+
+        for obj in self.types:
+            # read in the conversion sheet for xdc col name to flapjack name 
+                # for current obj
+            fj_conv_sht_obj = self.fj_conv_sht.loc[(self.fj_conv_sht['Sheet Name'] == obj)]
+            fj_conv_sht_obj = fj_conv_sht_obj.set_index('ColName').to_dict('index')
+
+            # read in current obj sheet
+            obj_df = self.xls.parse(obj, skiprows=0, index_col=f'{obj} ID')
+            cols = list(obj_df.columns)
+
+            # drop cols not used by fj and rename ones that are
+            new_cols = []
+            drop_cols = []
+            for col in cols:
+                if col in fj_conv_sht_obj.keys():
+                    new_cols.append(fj_conv_sht_obj[col]['FlapjackName'])
+                else:
+                    drop_cols.append(col)
+            
+            obj_df = obj_df.drop(columns=drop_cols)
+            obj_df.columns = new_cols
+            obj_df['object'] = obj
+            obj_df['flapjackid'] = ''
+
+            self.df = self.df.append(obj_df)
+
+    
+    def upload_studies(self, overwrite=False, confirm=False):
+        for index, row in self.df[self.df['object'] == 'Study'].iterrows():
+            fj_obj = self.fj.create(
+                'study',
+                name=row['name'],
+                description=row['description'],
+                confirm=confirm,
+                overwrite=overwrite,
+            )
+            self.hash_map[index] = fj_obj.id[0]
+            self.df.loc[index, 'flapjackid'] = fj_obj.id[0]
+            # if row[DOI] is not nan patch the DOI
+            if not pd.isna(row['DOI']):
+                self.fj.patch('study', fj_obj.id[0], doi=row['DOI'])
+
+
+
+    def upload_signals(self, overwrite=False, confirm=False):
+        for index, row in self.df[self.df['object'] == 'Signal'].iterrows():
+            fj_obj = self.fj.create(
+                'signal',
+                name=row['name'],
+                description=row['description'],
+                color=row['color'],
+                confirm=confirm,
+                overwrite=overwrite,
+            )
+            self.hash_map[index] = fj_obj.id[0]
+            self.df.loc[index, 'flapjackid'] = fj_obj.id[0]
+
+
+    def upload_chemicals(self, overwrite=False, confirm=False):
+        for index, row in self.df[self.df['object'] == 'Chemical'].iterrows():
+            fj_obj = self.fj.create(
+                'chemical',
+                name=row['name'],
+                description=row['description'],
+                confirm=confirm,
+                overwrite=overwrite,
+            )
+            self.hash_map[index] = fj_obj.id[0]
+            self.df.loc[index, 'flapjackid'] = fj_obj.id[0]
+
+    def upload_dna(self, overwrite=False, confirm=False):
+        for index, row in self.df[self.df['object'] == 'DNA'].iterrows():
+            fj_obj = self.fj.create(
+                'dna',
+                name=row['name'],
+                confirm=confirm,
+                overwrite=overwrite,
+            )
+            self.hash_map[index] = fj_obj.id[0]
+            self.df.loc[index, 'flapjackid'] = fj_obj.id[0]
+
+    
+    def upload_medias(self, overwrite=False, confirm=False):
+        for index, row in self.df[self.df['object'] == 'Media'].iterrows():
+            fj_obj = self.fj.create(
+                'media',
+                name=row['name'],
+                description=row['description'],
+                confirm=confirm,
+                overwrite=overwrite,
+            )
+            self.hash_map[index] = fj_obj.id[0]
+            self.df.loc[index, 'flapjackid'] = fj_obj.id[0]
+
+
+    def upload_strains(self, overwrite=False, confirm=False):
+        for index, row in self.df[self.df['object'] == 'Strain'].iterrows():
+            fj_obj = self.fj.create(
+                'strain',
+                name=row['name'],
+                description=row['description'],
+                confirm=confirm,
+                overwrite=overwrite,
+            )
+            self.hash_map[index] = fj_obj.id[0]
+            self.df.loc[index, 'flapjackid'] = fj_obj.id[0]
+
+
+    def upload_supplements(self, overwrite=False, confirm=False):
+        for index, row in self.df[self.df['object'] == 'Supplement'].iterrows():
+            fj_obj = self.fj.create(
+                'supplement',
+                name=row['name'],
+                description=row['description'],
+                chemical=self.df.loc[row['chemical'], 'flapjackid'],
+                concentration=row['concentration'],
+                confirm=confirm,
+                overwrite=overwrite,
+            )
+            self.hash_map[index] = fj_obj.id[0]
+            self.df.loc[index, 'flapjackid'] = fj_obj.id[0]
+
+
+    def upload_vectors(self, overwrite=False, confirm=False):
+        for index, row in self.df[self.df['object'] == 'Vector'].iterrows():
+            # print(self.df.loc[row['dna'], 'flapjackid'])
+            fj_obj = self.fj.create(
+                'vector',
+                name=row['name'],
+                description=row['description'],
+                dnas=[self.df.loc[row['dna'], 'flapjackid']],
+                confirm=confirm,
+                overwrite=overwrite,
+            )
+            self.hash_map[index] = fj_obj.id[0]
+            self.df.loc[index, 'flapjackid'] = fj_obj.id[0]
+
+
+    def upload_assays(self, overwrite=False, confirm=False):
+        for index, row in self.df[self.df['object'] == 'Assay'].iterrows():
+            fj_obj = self.fj.create(
+                'assay',
+                study=self.df.loc[row['study'], 'flapjackid'],
+                name=row['name'],
+                description=row['description'],
+                machine=row['machine'],
+                temperature=row['temperature'],
+                confirm=confirm,
+                overwrite=overwrite,
+            )
+            self.hash_map[index] = fj_obj.id[0]
+            self.df.loc[index, 'flapjackid'] = fj_obj.id[0]
+
+
+    def upload_samples(self, overwrite=False, confirm=False):
+        # get fj ids from sample design
+        for index, row in self.df[self.df['object'] == 'Sample'].iterrows():
+            media_index = self.df.loc[row['sampledesign'], 'media']
+            media_id = self.df.loc[media_index, 'flapjackid']
+            strain_index = self.df.loc[row['sampledesign'], 'strain']
+            strain_id = self.df.loc[strain_index, 'flapjackid']
+            vector_index = self.df.loc[row['sampledesign'], 'vector']
+            vector_id = self.df.loc[vector_index, 'flapjackid']
+
+            fj_obj = self.fj.create(
+                'sample',
+                assay=self.df.loc[row['assay'], 'flapjackid'],
+                media=media_id,
+                strain=strain_id,
+                vector=vector_id,
+                row=row['row'],
+                col=row['col'],
+                confirm=confirm,
+                overwrite=overwrite,
+            )
+            self.hash_map[index] = fj_obj.id[0]
+            self.df.loc[index, 'flapjackid'] = fj_obj.id[0]
+
+            if not pd.isna(row['supplement']):
+                supplement_index = self.df.loc[row['sampledesign'], 'supplement']
+                supplement_id = self.df.loc[supplement_index, 'flapjackid']
+                self.fj.patch('sample', fj_obj.id[0], supplements=supplement_id)
+
+
+
+
+
+
+# will use self.fj.create to create and upload data as fj objects in create_Type methods
+
+
